a8012e362932c48332c84ae732172384a2706c0e
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2021-07-15 00:41:33 -0300

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
+import { createGlobalStyle } from 'styled-components'
 import '../styles/globals.css'
+import {AlurakutStyles} from '../src/lib/AlurakutCommons'
 
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    min-height: auto;
+    flex-direction: column;
+  }
+
+  ${AlurakutStyles}
+`
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
